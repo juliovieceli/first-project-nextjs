756309e817aaaeb3ca2d3dfc0346dfdea0d661da
julio <juliovieceli@gmail.com>
2020-12-08 22:26:48 -0300

    .env/dinamic components/

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,7 @@
+export default function AddToCartModal() {
+   return (
+      <div>
+         do you really want to add this product to the cart?
+      </div>
+   )
+}
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,10 +1,31 @@
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+//import AddToCartModal from '../../../components/AddToCartModal'
+
+const AddToCartModal = dynamic(
+   () => import('@/components/AddToCartModal'),
+   { loading: () => <p>Carregando...</p>, ssr: false }
+
+)
 
 export default function Search() {
-
    const router = useRouter()
+   const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
+
+   function handleAddToCart() {
+      console.log(process.env.NEXT_PUBLIC_API_URL)
+      setIsAddToCartModalVisible(true)
+   }
 
    return (
-      <h1>{router.query.slug}</h1>
+      <div>
+         <h1>{router.query.slug}</h1>
+
+         <button onClick={handleAddToCart}>Add to cart</button>
+
+         {isAddToCartModalVisible && <AddToCartModal />}
+      </div>
+
    )
 }
